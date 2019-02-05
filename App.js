f03dab8ea624db1c938dc8754f682f37f27764af
dev <dev@gossip.us-east1-b.c.fabric-220111.internal>
2019-02-05 02:46:43 +0000

    adapt to react.gradle

--- App.js
+++ App.js
@@ -23,6 +23,7 @@ export default class App extends Component<Props> {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.welcome}>G G</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
